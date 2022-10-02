b035e673c60b32914b66d1447e96da2ffa0abb31
Adriano Orioli <orioli.adriano@gmail.com>
2022-10-02 15:52:03 +0200

    add boneblock ghost

--- game/Main.cs
+++ game/Main.cs
@@ -1,10 +1,16 @@
 using Godot;
+using GodotOnReady.Attributes;
 using System;
 
 namespace Game
 {
-    public class Main : Node
+    public partial class Main : Node
     {
 
+        [OnReadyGet("UI")]
+        private CanvasLayer ui;
+
+        [OnReady]
+        private void SetUIVisible() => ui.Show();
     }
 }
